e631d1d505c8d4c1e67667fde856b207cd231e5c
João Henrique <joaohenrique.hs@hotmail.com>
2020-07-14 22:33:15 -0300

    chore: Config CORS

--- src/shared/infra/http/server.ts
+++ src/shared/infra/http/server.ts
@@ -1,13 +1,15 @@
 import 'reflect-metadata';
 
 import express from 'express';
+import cors from 'cors';
 import routes from './routes';
 
 import '@shared/infra/typeorm';
 
 const app = express();
 
-app.use(routes);
+app.use(cors());
 app.use(express.json());
+app.use(routes);
 
 app.listen(process.env.PORT || 3333);
