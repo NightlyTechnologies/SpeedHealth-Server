16f486fecc6fd1b9f03e43997f587cdc5f6dcdd4
João Henrique <joaohenrique.hs@hotmail.com>
2020-07-18 18:56:39 -0300

    work: Add middleware on create pharmacys

--- src/modules/pharmacys/infra/http/routes/pharmacys.routes.ts
+++ src/modules/pharmacys/infra/http/routes/pharmacys.routes.ts
@@ -1,4 +1,5 @@
 import { Router } from 'express';
+import { celebrate, Segments, Joi } from 'celebrate';
 
 import PharmacysController from '../controllers/PharmacysController';
 
@@ -6,6 +7,17 @@ const router = Router();
 
 const pharmacysController = new PharmacysController();
 
-router.post('/', pharmacysController.create);
+router.post('/', celebrate({
+  [Segments.BODY]: {
+    name: Joi.string().required(),
+    email: Joi.string().email().required(),
+    password: Joi.string().required().min(5),
+    whatsapp: Joi.number().positive().required(),
+    city: Joi.string().required(),
+    uf: Joi.string().required().length(2),
+    cnpj: Joi.number().positive().required(),
+    geolocation: Joi.array().length(2).required(),
+  },
+}), pharmacysController.create);
 
 export default router;
