566702aaef981d3a99bc80999caec4f2770d6024
João Henrique <joaohenrique.hs@hotmail.com>
2020-07-23 14:11:12 -0300

    work: Config jwt generation

--- src/shared/container/index.ts
+++ src/shared/container/index.ts
@@ -2,6 +2,7 @@ import { container } from 'tsyringe';
 
 import IPharmacysRepository from '../../modules/pharmacys/repositories/IPharmacysRepository';
 import PharmacysRepository from '../../modules/pharmacys/infra/typeorm/repositories/PharmacysRepository';
+import '@modules/pharmacys/providers';
 
 container.registerSingleton<IPharmacysRepository>(
   'PharmacysRepository',
