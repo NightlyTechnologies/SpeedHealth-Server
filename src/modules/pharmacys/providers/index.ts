465fcd45f5d8057619be9f59d3b9ffb5fe9687af
João Henrique <joaohenrique.hs@hotmail.com>
2020-07-24 16:59:53 -0300

    feat: Add Argon2 provider

--- src/modules/pharmacys/providers/index.ts
+++ src/modules/pharmacys/providers/index.ts
@@ -1,6 +1,6 @@
 import { container } from 'tsyringe';
 
 import IHashProvider from './HashProvider/models/IHashProvider';
-import BCryptHashProvider from './HashProvider/implementations/BCryptHashProvider';
+import Argon2HashProvider from './HashProvider/implementations/Argon2HashProvider';
 
-container.registerSingleton<IHashProvider>('HashProvider', BCryptHashProvider);
+container.registerSingleton<IHashProvider>('HashProvider', Argon2HashProvider);
