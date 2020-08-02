0edc0a782794062bb0700b14989665100679795f
João Henrique <joaohenrique.hs@hotmail.com>
2020-08-01 22:50:11 -0300

    feat(http): Route to update pharmacy

--- src/modules/pharmacys/infra/http/controllers/PharmacysController.ts
+++ src/modules/pharmacys/infra/http/controllers/PharmacysController.ts
@@ -4,8 +4,35 @@ import { classToClass } from 'class-transformer';
 
 import CreatePharmacyService from '@modules/pharmacys/services/CreatePharmacyService';
 import ListAllPharmacysService from '@modules/pharmacys/services/ListAllPharmacysService';
+import UpdatePharmacyService from '@modules/pharmacys/services/UpdatePhamacyService';
+import IUpdatePharmacyDTO from '@modules/pharmacys/dtos/IUpdatePharmacyDTO';
 
 export default class PharmacyController {
+  public async update(request: Request, response: Response): Promise<Response> {
+    const updatePharmacyService = container.resolve(UpdatePharmacyService);
+
+    const { id } = request.pharmacy;
+
+    const {
+      name, email, whatsapp, city, uf, cnpj, geolocation,
+    } = request.body;
+
+    const data: IUpdatePharmacyDTO = {
+      id,
+      name,
+      email,
+      whatsapp,
+      city,
+      uf,
+      cnpj,
+      geolocation,
+    };
+
+    const pharmacy = await updatePharmacyService.execute(data);
+
+    return response.json(pharmacy);
+  }
+
   public async index(_request: Request, response: Response): Promise<Response> {
     const listPharmacys = container.resolve(ListAllPharmacysService);
 
